0be26c33b36e5dae0babafc1387a3ee918e6b64b
Ярослав Аверин <averinj@mail.ru>
2024-03-25 22:14:41 +0300

    init

---
+++ client/src/routes/index.ts
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "../pages/Home";
+
+export interface IRoute {
+	index?: boolean;
+	path: string;
+	component: React.ComponentType;
+}
+
+export enum RouteNames {
+	HOME = "/",
+}
+
+export const SiteRoutes: IRoute[] = [{ index: true, path: RouteNames.HOME, component: Home }];
