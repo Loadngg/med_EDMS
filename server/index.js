6e5706389f4ec4b5bb8960d2799b346dd6282e64
Ярослав Аверин <averinj@mail.ru>
2024-04-04 02:44:37 +0300

    + added models.js > backend renamed to server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models");
+const path = require("path");
+const fileUpload = require("express-fileupload");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 
 const start = async () => {
 	try {
+		await sequelize.authenticate();
+		await sequelize.sync();
 		app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
 	} catch (e) {
 		console.log(e);
---
+++ server/models/models.js
@@ -0,0 +1,66 @@
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
+
+const Cabinet = sequelize.define("cabinet", {
+	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+	floor: { type: DataTypes.INTEGER },
+	number: { type: DataTypes.INTEGER, allowNull: false },
+});
+
+const Profile = sequelize.define("profile", {
+	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+	name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const Doctor = sequelize.define("doctor", {
+	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+	fullName: { type: DataTypes.STRING, allowNull: false },
+	receptionTimeStart: { type: DataTypes.TIME, allowNull: false },
+	receptionTimeEnd: { type: DataTypes.TIME, allowNull: false },
+	img: { type: DataTypes.STRING },
+});
+
+const Client = sequelize.define("client", {
+	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+	fullName: { type: DataTypes.STRING, allowNull: false },
+	birthday: { type: DataTypes.DATEONLY, allowNull: false },
+	idCard: { type: DataTypes.INTEGER, allowNull: false },
+	policy: { type: DataTypes.INTEGER, allowNull: false },
+});
+
+const Reason = sequelize.define("reason", {
+	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+	name: { type: DataTypes.STRING, unique: true, allowNull: false },
+	urgency: { type: DataTypes.FLOAT, allowNull: false },
+	noWaiting: { type: DataTypes.BOOLEAN, defaultValue: false },
+});
+
+const Reception = sequelize.define("reception", {
+	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+	receptionTimeStart: { type: DataTypes.TIME, allowNull: false },
+	receptionTimeEnd: { type: DataTypes.TIME, allowNull: false },
+});
+
+Cabinet.hasMany(Doctor);
+Doctor.belongsTo(Cabinet);
+
+Profile.hasMany(Doctor);
+Doctor.belongsTo(Profile);
+
+Doctor.hasMany(Reception);
+Reception.belongsTo(Doctor);
+
+Client.hasMany(Reception);
+Reception.belongsTo(Client);
+
+Reason.hasMany(Reception);
+Reception.belongsTo(Reason);
+
+module.exports = {
+	Cabinet,
+	Profile,
+	Doctor,
+	Client,
+	Reason,
+	Reception,
+};
